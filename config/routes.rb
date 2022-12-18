41b8febba70cc24ac120c5d4d9f7ca08252c40ad
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2022-12-18 07:00:55 +0000

    [finish]4-9 resources post_images only new show edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
+  resources :post_images,only:[:new,:index,:show]
   devise_for :users
   root to: 'homes#top'
   get'homes/about'=> 'homes#about',as: 'about'
