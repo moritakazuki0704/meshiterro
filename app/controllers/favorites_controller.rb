3f8428ffeeeee20016014f755f5e7b92e35ee66b
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2023-01-07 00:41:51 +0000

    [add]4-19 add favorite function to show screen fix misspelled in favorites_controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,8 +6,8 @@ class FavoritesController < ApplicationController
     redirect_to post_image_path(post_image)
   end
   def destroy
-    post_image = PostImage.find(params[:past_image_id])
-    favorite = current_user.favorites.find_by(post_imgae_id: post_image.id)
+    post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
     redirect_to post_image_path(post_image)
   end
